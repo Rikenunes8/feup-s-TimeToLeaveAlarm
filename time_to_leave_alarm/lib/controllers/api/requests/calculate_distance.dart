740fd70065551960f54afc15b2c87c604a05ea15
Filipe Campos <filipepcampos24@gmail.com>
2023-12-30 02:25:47 +0000

    start migration to Routes API

--- time_to_leave_alarm/lib/controllers/api/requests/calculate_distance.dart
+++ time_to_leave_alarm/lib/controllers/api/requests/calculate_distance.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:time_to_leave_alarm/controllers/api/secrets.dart';
+import 'package:time_to_leave_alarm/controllers/api/requests/address_to_coordinates.dart';
 import 'package:http/http.dart' as http;
 
 calculateDistance(
@@ -12,22 +13,45 @@ calculateDistance(
     debugPrint('Google Distance API Key not configured. Using mock data');
     then(2342);
   } else {
-    final queryParameters = {
-      'destinations': destination,
-      'origins': origin,
-      'mode': 'driving',
+    final uriParameters = {
       'key': googleDistanceAPIKey,
+      'fields': 'routes.distanceMeters,routes.duration',
     };
-    final uri = Uri.https('maps.googleapis.com',
-        '/maps/api/distancematrix/json', queryParameters);
+    final queryParameters = {
+      'destination': {
+        'address': destination,
+      },
+      'origin': {
+        'address': origin,
+      },
+      // 'intermediates': [
+      //   {
+      //     'location': {
+      //       'address': 'Faculdade de Engenharia da Universidade do Porto',
+      //     }
+      //    'via': true/false
+      //   }
+      // ],
+
+      'travelMode': 'DRIVE',
+      'routeModifiers': {
+        'avoidTolls': false,
+        'avoidHighways': false,
+        'avoidFerries': false
+      },
+      // "departureTime": "2023-10-15T15:01:23.045123456Z",
+    };
+    // final uri = Uri.https('maps.googleapis.com',
+    //     '/maps/api/distancematrix/json', queryParameters);
+    final uri = Uri.https(
+        'routes.googleapis.com', '/directions/v2:computeRoutes', uriParameters);
     debugPrint(uri.toString());
-    http.get(uri).then((value) {
+    http.post(uri, body: jsonEncode(queryParameters)).then((value) {
       debugPrint(value.body);
       debugPrint(value.statusCode.toString());
       var json = jsonDecode(value.body);
-      var element = json["rows"][0]["elements"][0];
-      var time = element["duration"]
-          ["value"]; // duration_in_traffic is not always available
+      var time = json["routes"][0]["duration"];
+
       then(time);
     });
   }
