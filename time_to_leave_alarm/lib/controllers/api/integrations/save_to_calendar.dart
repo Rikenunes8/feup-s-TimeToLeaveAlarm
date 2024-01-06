3f06bf57ba86e0a6294bb3745320b1f3c45b860d
Pedro Gonçalo Correia <goncalerta@gmail.com>
2024-01-06 19:19:39 +0000

    Save periodic alarms to calendar

--- time_to_leave_alarm/lib/controllers/api/integrations/save_to_calendar.dart
+++ time_to_leave_alarm/lib/controllers/api/integrations/save_to_calendar.dart
@@ -3,6 +3,37 @@ import 'package:time_to_leave_alarm/models/alarm.dart';
 import 'package:time_to_leave_alarm/controllers/utils.dart';
 import 'package:add_2_calendar/add_2_calendar.dart';
 
+getWeekDays(String data) {
+  var days = data.split(',');
+  var weekDays = '';
+  for (var day in days) {
+    switch (day) {
+      case '0':
+        weekDays += 'MO,';
+        break;
+      case '1':
+        weekDays += 'TU,';
+        break;
+      case '2':
+        weekDays += 'WE,';
+        break;
+      case '3':
+        weekDays += 'TH,';
+        break;
+      case '4':
+        weekDays += 'FR,';
+        break;
+      case '5':
+        weekDays += 'SA,';
+        break;
+      case '6':
+        weekDays += 'SU,';
+        break;
+    }
+  }
+  return weekDays.substring(0, weekDays.length - 1);
+}
+
 saveToCalendar({required Alarm alarm}) {
   final Event event = Event(
     title:
@@ -11,8 +42,10 @@ saveToCalendar({required Alarm alarm}) {
     startDate: stringToDateTime(alarm.leaveTime),
     endDate: stringToDateTime(alarm.arriveTime),
     allDay: false,
+    recurrence: alarm.period
+        ? Recurrence(frequency: null, rRule: "FREQ=WEEKLY;BYDAY=${getWeekDays(alarm.periodData)};INTERVAL=1")
+        : null,
   );
-  // TODO: Deal with recurrence
 
   Add2Calendar.addEvent2Cal(event);
 }
